7ec4e1ed6a848db73d03d4d5ac103d40a1e7e8c6
Michael Bastos <bastosmichael@gmail.com>
2015-10-17 05:23:24 -0700

    adding indexing and pending to homepage

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,7 @@ class VisitorsController < ApplicationController
   def index
     counts = Pricels::Counts.new
     @available = counts.available
+    @indexing = counts.indexing
     @processing = counts.processing
     @pending = counts.pending
   end
