dcf653d283ca0a753093cc96ffc7b1941d866ff6
Michael Bastos <bastosmichael@gmail.com>
2016-02-15 13:39:12 -0800

    getting amazon ads working

---
+++ app/views/layouts/_amazon_ads_search.rb
@@ -0,0 +1,16 @@
+.container.text-center
+  %script{:src => "//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"}
+  :javascript
+    amzn_assoc_placement = "adunit0";
+    amzn_assoc_search_bar = "false";
+    amzn_assoc_tracking_id = "pricelscom-20";
+    amzn_assoc_search_bar_position = "bottom";
+    amzn_assoc_ad_mode = "search";
+    amzn_assoc_ad_type = "smart";
+    amzn_assoc_marketplace = "amazon";
+    amzn_assoc_region = "US";
+    amzn_assoc_title = "Sponsored";
+    amzn_assoc_default_search_phrase = "#{params[:query]}";
+    amzn_assoc_default_category = "All";
+    amzn_assoc_textlinks = "";
+    amzn_assoc_linkid = "cc7dbc777bb0021ea29865c5c353be17";
