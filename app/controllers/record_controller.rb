9db1232475894b66653ba38bffd3b9ec690d309b
Michael Bastos <bastosmichael@gmail.com>
2016-02-23 21:03:15 -0800

    adding record controller

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,5 +1,18 @@
 class RecordController < ApplicationController
   def index
+    if params[:container].presence && params[:record_id].presence
+      @record = object.record
+      render_404 if @record[:error]
+    end
+  end
 
+  protected
+
+  def render_404
+    render(file: "#{Rails.root}/public/404.html", layout: false, status: 404)
+  end
+
+  def object
+    @object ||= Pricels::Record.new(params[:container], params[:record_id])
   end
 end
