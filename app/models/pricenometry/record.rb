2ef4dd4d02ce0624697ae4817f29a447423de4ae
Michael Bastos <bastosmichael@gmail.com>
2016-04-20 05:53:50 -0700

    fixing problems

--- app/models/pricenometry/record.rb
+++ app/models/pricenometry/record.rb
@@ -13,35 +13,49 @@ class Pricenometry::Record < Pricenometry::Base
     @options = @options.merge(fetch: crawl)
     url
     json[:result]
+  rescue
+    nil
   end
 
   def history
     url 'history'
     json[:result][:history]
+  rescue
+    nil
   end
 
   def related
     url 'related'
     json[:result][:related]
+  rescue
+    nil
   end
 
   def videos
     url 'videos'
     json[:result][:videos]
+  rescue
+    nil
   end
 
   def news
     url 'news'
     json[:result][:news]
+  rescue
+    nil
   end
 
   def references
     url 'references'
     json[:result][:references]
+  rescue
+    nil
   end
 
   def links
     url 'links'
     json[:result][:links]
+  rescue
+    nil
   end
 end
