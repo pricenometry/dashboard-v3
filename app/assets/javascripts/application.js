c0f327d733abe1368a935b97e35aa8dbb5cb6ca5
Michael Bastos <bastosmichael@gmail.com>
2016-06-09 04:23:49 -0700

    adding facebook ads

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,26 @@
 GaEvents.Event.adapter = function() {
   return new GaEvents.GoogleUniversalAnalyticsAdapter();
 }
+
+ window.fbAsyncInit = function() {
+        FB.Event.subscribe(
+          'ad.loaded',
+          function(placementId) {
+            console.log('Audience Network ad loaded');
+            document.getElementById('ad_root').style.display = 'block';
+          }
+        );
+        FB.Event.subscribe(
+          'ad.error',
+          function(errorCode, errorMessage, placementId) {
+            console.log('Audience Network error (' + errorCode + ') ' + errorMessage);
+          }
+        );
+      };
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=1008307585917302";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
