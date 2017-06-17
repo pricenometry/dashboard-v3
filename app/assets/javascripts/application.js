5fcb8073196af6fc1ed404ac6fb4b9f3c6837596
Michael Bastos <bastosmichael@gmail.com>
2017-06-17 08:06:37 -0700

    updating design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,36 +10,36 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
-//= require ga_events.js
-//= require_tree .
+// //= require jquery
+// //= require jquery_ujs
+// //= require turbolinks
+// //= require bootstrap-sprockets
+// //= require ga_events.js
+// //= require_tree .
 
-GaEvents.Event.adapter = function() {
-  return new GaEvents.GoogleUniversalAnalyticsAdapter();
-}
+// GaEvents.Event.adapter = function() {
+//   return new GaEvents.GoogleUniversalAnalyticsAdapter();
+// }
 
- window.fbAsyncInit = function() {
-        FB.Event.subscribe(
-          'ad.loaded',
-          function(placementId) {
-            console.log('Audience Network ad loaded');
-            document.getElementById('ad_root').style.display = 'block';
-          }
-        );
-        FB.Event.subscribe(
-          'ad.error',
-          function(errorCode, errorMessage, placementId) {
-            console.log('Audience Network error (' + errorCode + ') ' + errorMessage);
-          }
-        );
-      };
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=1008307585917302";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+//  window.fbAsyncInit = function() {
+//         FB.Event.subscribe(
+//           'ad.loaded',
+//           function(placementId) {
+//             console.log('Audience Network ad loaded');
+//             document.getElementById('ad_root').style.display = 'block';
+//           }
+//         );
+//         FB.Event.subscribe(
+//           'ad.error',
+//           function(errorCode, errorMessage, placementId) {
+//             console.log('Audience Network error (' + errorCode + ') ' + errorMessage);
+//           }
+//         );
+//       };
+//       (function(d, s, id) {
+//         var js, fjs = d.getElementsByTagName(s)[0];
+//         if (d.getElementById(id)) return;
+//         js = d.createElement(s); js.id = id;
+//         js.src = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=1008307585917302";
+//         fjs.parentNode.insertBefore(js, fjs);
+//       }(document, 'script', 'facebook-jssdk'));
