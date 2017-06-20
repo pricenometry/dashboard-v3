042448e28604949086d16ab2cd83e429fd39efe3
Michael Bastos <bastosmichael@gmail.com>
2017-06-20 07:48:38 -0700

    fixing new design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
 //= require bootstrap-sprockets
 //= require jquery-ui
 //= require jquery_ujs
